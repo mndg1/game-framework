b0004d5a8c5f807749562dcd966dac37a1c44f01
mndg <mannes.dg@gmail.com>
2023-10-15 12:26:34 +0200

    Arena things

--- GameFramework/Arena/src/main/java/me/kap/gfw/arena/ArenaLocation.java
+++ GameFramework/Arena/src/main/java/me/kap/gfw/arena/ArenaLocation.java
@@ -2,6 +2,9 @@ package me.kap.gfw.arena;
 
 import org.bukkit.Location;
 
+/**
+ * A class to represent a {@link Location} which is tied to a name.
+ */
 public class ArenaLocation {
     private final String locationName;
     private final Location bukkitLocation;
