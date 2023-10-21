f5c246630fdd70ce3bea4e333707e83e477bf4d5
mndg <mannes.dg@gmail.com>
2023-10-21 15:22:32 +0200

    docs

--- GameFramework/Events/src/main/java/me/kap/gfw/events/timing/TimerComponent.java
+++ GameFramework/Events/src/main/java/me/kap/gfw/events/timing/TimerComponent.java
@@ -3,6 +3,9 @@ package me.kap.gfw.events.timing;
 import me.kap.gfw.game.GameComponent;
 import org.bukkit.plugin.Plugin;
 
+/**
+ * A {@link GameComponent} which adds functionality for scheduling events.
+ */
 public class TimerComponent extends GameComponent {
     private final Plugin plugin;
     private final EventTimer eventTimer = new EventTimer();
