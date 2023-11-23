23305c302389305b4c5b51805faa02242a3891d5
mndg <mannes.dg@gmail.com>
2023-11-23 23:05:12 +0100

    Remove update method

--- GameFramework/Game/src/main/java/me/kap/gfw/game/Game.java
+++ GameFramework/Game/src/main/java/me/kap/gfw/game/Game.java
@@ -45,11 +45,6 @@ public abstract class Game {
     protected abstract void setup();
 
     /**
-     * Executed at each game tick. Used for implementing repeating logic.
-     */
-    protected abstract void update();
-
-    /**
      * Executed at the end of a game. Used for implementing custom ending logic.
      */
     protected abstract void finish();
