6dafde3ed0c1efa63f0325b1a142e88463706989
mndg <mannes.dg@gmail.com>
2024-01-16 17:47:08 +0100

    Redesign how events are represented

--- GameFramework/Events/src/main/java/me/kap/gfw/events/timing/TimerComponent.java
+++ GameFramework/Events/src/main/java/me/kap/gfw/events/timing/TimerComponent.java
@@ -3,23 +3,15 @@ package me.kap.gfw.events.timing;
 import me.kap.gfw.game.GameComponent;
 import org.bukkit.plugin.Plugin;
 
-import java.time.Clock;
-
 /**
  * A {@link GameComponent} which adds functionality for scheduling events.
  */
 public class TimerComponent extends GameComponent {
     private final Plugin plugin;
     private final EventTimer eventTimer = new EventTimer();
-    private final Clock clock;
 
     public TimerComponent(Plugin plugin) {
-        this(plugin, Clock.systemUTC());
-    }
-
-    public TimerComponent(Plugin plugin, Clock clock) {
         this.plugin = plugin;
-        this.clock = clock;
     }
 
     @Override
@@ -34,9 +26,5 @@ public class TimerComponent extends GameComponent {
 
     public EventTimer getEventTimer() {
         return eventTimer;
-    }
-
-    public Clock getClock() {
-        return clock;
     }
 }
