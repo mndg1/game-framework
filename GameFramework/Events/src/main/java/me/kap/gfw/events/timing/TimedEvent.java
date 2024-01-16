f76196e4ebd21a136f20e845b6a77d086246af75
mndg <mannes.dg@gmail.com>
2024-01-16 17:53:37 +0100

    Limit access to TimedEvent

--- GameFramework/Events/src/main/java/me/kap/gfw/events/timing/TimedEvent.java
+++ GameFramework/Events/src/main/java/me/kap/gfw/events/timing/TimedEvent.java
@@ -5,5 +5,5 @@ import java.time.Duration;
 /**
  * Represents an event that can execute at a given time.
  */
-public record TimedEvent(Runnable callback, boolean repeating, Duration interval) {
+record TimedEvent(Runnable callback, boolean repeating, Duration interval) {
 }
