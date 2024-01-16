8a0db441f627bf5fb8c23288b53b0e40c704d942
mndg <mannes.dg@gmail.com>
2024-01-16 16:24:06 +0100

    Add Clock to TimerComponent

--- GameFramework/Events/src/main/java/me/kap/gfw/events/timing/TimerComponent.java
+++ GameFramework/Events/src/main/java/me/kap/gfw/events/timing/TimerComponent.java
@@ -3,15 +3,23 @@ package me.kap.gfw.events.timing;
 import me.kap.gfw.game.GameComponent;
 import org.bukkit.plugin.Plugin;
 
+import java.time.Clock;
+
 /**
  * A {@link GameComponent} which adds functionality for scheduling events.
  */
 public class TimerComponent extends GameComponent {
     private final Plugin plugin;
     private final EventTimer eventTimer = new EventTimer();
+    private final Clock clock;
 
     public TimerComponent(Plugin plugin) {
+        this(plugin, Clock.systemUTC());
+    }
+
+    public TimerComponent(Plugin plugin, Clock clock) {
         this.plugin = plugin;
+        this.clock = clock;
     }
 
     @Override
@@ -26,5 +34,9 @@ public class TimerComponent extends GameComponent {
 
     public EventTimer getEventTimer() {
         return eventTimer;
+    }
+
+    public Clock getClock() {
+        return clock;
     }
 }
