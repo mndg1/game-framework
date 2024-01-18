21cf8d583a540efe6a16b084a9fd01ff0edaf942
MNDG <61918019+mndg1@users.noreply.github.com>
2024-01-18 14:51:27 +0100

    Use Comparators.comparing instead of custom comparator (#13)

--- GameFramework/Events/src/main/java/me/kap/gfw/events/timing/ScheduledEvent.java
+++ GameFramework/Events/src/main/java/me/kap/gfw/events/timing/ScheduledEvent.java
@@ -1,15 +1,7 @@
 package me.kap.gfw.events.timing;
 
-import java.util.Comparator;
-
 /**
  * Represents an event that has been scheduled to execute at a given time.
  */
 record ScheduledEvent(long executionTime, TimedEvent event) {
-    static class ExecutionTimeComparator implements Comparator<ScheduledEvent> {
-        @Override
-        public int compare(ScheduledEvent o1, ScheduledEvent o2) {
-            return (int) (o1.executionTime() - o2.executionTime());
-        }
-    }
 }
