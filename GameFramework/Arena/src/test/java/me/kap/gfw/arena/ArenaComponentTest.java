77a9aaf006f822757063e7e8b082571589fc07e0
mndg <mannes.dg@gmail.com>
2024-01-19 14:38:47 +0100

    Update arena test names

--- GameFramework/Arena/src/test/java/me/kap/gfw/arena/ArenaComponentTest.java
+++ GameFramework/Arena/src/test/java/me/kap/gfw/arena/ArenaComponentTest.java
@@ -15,13 +15,13 @@ class ArenaComponentTest {
     private final ArenaComponent arenaComponent = new ArenaComponent(List.of(requiredLocationName), new Arena("test-arena"));
 
     @Test
-    void whenStart_withMissingRequiredLocations_thenExceptionIsThrown() {
+    void whenValidateStart_withMissingRequiredLocations_thenExceptionIsThrown() {
         // assert
         assertThrows(GameStateChangeException.class, arenaComponent::validateStart);
     }
 
     @Test
-    void whenStart_withNoMissingRequiredLocations_thenStartReturnsTrue() {
+    void whenValidateStart_withNoMissingRequiredLocations_thenStartReturnsTrue() {
         // arrange
         var arenaLocationFake = mock(ArenaLocation.class);
         when(arenaLocationFake.locationName()).thenReturn(requiredLocationName);
