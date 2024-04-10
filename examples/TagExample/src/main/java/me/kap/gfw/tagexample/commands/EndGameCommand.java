c361711132705b436376859eff53d57579642b01
mndg <mannes.dg@gmail.com>
2024-04-10 20:25:33 +0200

    Remove redundant imports

--- examples/TagExample/src/main/java/me/kap/gfw/tagexample/commands/EndGameCommand.java
+++ examples/TagExample/src/main/java/me/kap/gfw/tagexample/commands/EndGameCommand.java
@@ -2,8 +2,6 @@ package me.kap.gfw.tagexample.commands;
 
 import me.kap.gfw.game.exceptions.GameStateChangeException;
 import me.kap.gfw.tagexample.game.TagGame;
-import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.ComponentBuilder;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
